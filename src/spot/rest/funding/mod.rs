646d0bf46893c1bb4f27054406454e1cc937b3aa
Jaiden Woods <mrteathyme@gmail.com>
2025-04-04 07:01:24 +1100

    Implemented withdrawal api (untested)

--- src/spot/rest/funding/mod.rs
+++ src/spot/rest/funding/mod.rs
@@ -1,3 +1,6 @@
+use serde::Serialize;
+use serde_with::{serde_as, DisplayFromStr};
+
 use crate::{KrakenRequest, APIKey, APISecret};
 use crate::spot::rest::{Payload, sign};
 
@@ -7,6 +10,82 @@ pub enum BoolUnion<T> {
     Bool(bool),
     Data(T)
 }
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct ReferenceID {
+    pub refid: String 
+}
+
+//ToDo: Move this to lib for easier importing (or maybe a prelude or reexport?)
+//ToDo: Implement sanity check by changing address to a network enum that checks for malformed
+//address on creation
+//checking if the asset supports the network would also probably be nice but given the number of
+//assets available might be better to do that at runtime (I believe you can get that information
+//from the api)
+#[derive(Serialize, Clone, Debug)]
+pub struct KrakenWithdrawalAddress<'a> {
+    pub address: &'a str,
+    pub key: &'a str,
+    pub asset: &'a str
+}
+
+impl<'a> KrakenWithdrawalAddress<'a> {
+    pub fn new(asset: &'a str, address: &'a str, key: &'a str) -> Self {
+        KrakenWithdrawalAddress {asset, address, key }
+    }
+}
+
+pub fn withdraw(key: &APIKey, secret: &APISecret, nonce: i64, address: KrakenWithdrawalAddress, amount: f64, max_fee: Option<f64>) -> KrakenRequest<ReferenceID> { //Todo: model asset and asset_class as types
+    #[serde_as]
+    #[derive(serde::Serialize)]
+    struct Parameters<'a> {
+        nonce: i64,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[serde_as(as = "Option<DisplayFromStr>")]
+        max_fee: Option<f64>,
+        #[serde(flatten)]
+        address: KrakenWithdrawalAddress<'a>,
+        #[serde_as(as = "DisplayFromStr")]
+        amount: f64,
+    }
+    impl<'a> Payload for Parameters<'a> {
+        fn nonce(&self) -> i64 {
+            self.nonce
+        }
+    }
+    let params = Parameters {
+        nonce,
+        address,
+        amount,
+        max_fee
+    };
+    let uri = http::uri::Uri::from_static("https://api.kraken.com/0/private/Withdraw");
+    let path = uri.path_and_query().unwrap();
+    let request = http::request::Builder::new()
+            .method("POST")
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .header("API-Key", key.to_string())
+            .header("API-Sign", sign(secret,path.clone(),&params).to_string())
+            .uri(uri)
+            .body(serde_json::to_string(&params).unwrap()).unwrap();
+    KrakenRequest::new(request)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #[derive(serde::Deserialize, Clone, Debug)]
 pub struct DepositMethod {
